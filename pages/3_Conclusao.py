dca6e0a8b2df400f209379db236c3e8988d5b9c6
GuilhermeTMartins <138918193+GuilhermeTMartins@users.noreply.github.com>
2026-05-28 19:36:01 -0300

    Add conclusion page with analysis of school dropout data

---
+++ pages/3_Conclusao.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Conclusão")
+
+st.write("""
+A análise dos dados permitiu identificar padrões relevantes relacionados à evasão escolar no Brasil.
+
+Os indicadores demonstraram diferenças significativas entre regiões e redes de ensino, além de variações temporais importantes na taxa de evasão.
+
+Os gráficos e KPIs desenvolvidos auxiliam na compreensão do problema e podem apoiar processos de tomada de decisão voltados para políticas educacionais e estratégias de permanência escolar.
+""")
